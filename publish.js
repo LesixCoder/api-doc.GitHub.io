5c6c3877b95339414f59bedc2acf9c02956e03e5
liusixin <liusixin@tal.com>
2021-04-12 17:27:36 +0800

    chore: 更新文档

---
+++ publish.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/* 发布文档到本仓库的gh-pages分支 */
+const execSync = require('child_process').execSync
+const ghpages = require('gh-pages')
+const VERSION = require('./package.json').version
+const GIT_COMMIT = execSync('git rev-parse --short HEAD').toString().replace(/\n/, '')
+
+ghpages.publish('docs', {
+  message: `[deploy] ${GIT_COMMIT} - [release] ${VERSION}`
+})
